ef93870238cba52e6f6674c7666a271227a0a180
NIschay <parag.j@pathpartnertech.com>
2020-05-22 21:55:28 +0530

    adding files meta data commmets templates in few files and starting with webrtc

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
+/**
+  ===========================================================================
+ * @file: App.js
+ *
+ * @brief: This file contains main page which holds navigator for other pics
+ *
+ * ============================================================================
+ *
+ * Copyright © Eniacoder, 2020
+ *
+ * This material, including documentation and any related computer programs,is
+ * protected by copyright controlled by Eniacoder.All rights are
+ * reserved.Copying, including reproducing,storing, adapting or translating,any
+ * or all of this material require written consent of Eniacoder.
+ * This material may also contain confidential information,which should not be
+ * disclosed to others without prior written consent of Eniacoder.
+ *
+ * ============================================================================
+ *
+ * <b> REVISION HISTORY </b>
+ *
+ * @Version 1.0 : 22-MAY-2019 
+ * Author :Parag Jain 
+ *                                          
+ *
+ *
+ *============================================================================
+ */
+
 //import libraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button,Alert } from 'react-native';
@@ -11,6 +40,8 @@ import { JoinRoomScreen } from './Pages/JoinRoomScreen';
 import { TossScreen } from './Pages/TossScreen'
 import { SpinnerScreen } from './Pages/SpineerScreen2'
 import {Provider} from './context/Provider'
+import {AudioScreen} from './Pages/AudioScreen'
+import {WebRtcScreen} from './Pages/WebRtcScreen'
 
 //Naviagtion Control Stack
 const Stack = createStackNavigator();
@@ -30,6 +61,7 @@ function App() {
       <Stack.Screen name="JoinRoomScreen" component={JoinRoomScreen}  />
       <Stack.Screen name="TossScreen" component={TossScreen}  />
       <Stack.Screen name="SpinnerScreen" component={SpinnerScreen}  />
+      <Stack.Screen name="WebRtcScreen" component={WebRtcScreen}  />
       </Stack.Navigator>
     </NavigationContainer>
     </Provider>
--- Pages/MainScreen.js
+++ Pages/MainScreen.js
@@ -1,3 +1,35 @@
+/**
+  ===========================================================================
+ * @file: Mainscreen.js
+ *
+ * @brief: Mainscreen from where we will navigate to other componenet of application
+ *
+ *
+ * ============================================================================
+ *
+ * Copyright © Eniacoder, 2020
+ *
+ * This material, including documentation and any related computer programs,is
+ * protected by copyright controlled by Eniacoder.All rights are
+ * reserved.Copying, including reproducing,storing, adapting or translating,any
+ * or all of this material require written consent of Eniacoder.
+ * This material may also contain confidential information,which should not be
+ * disclosed to others without prior written consent of Eniacoder.
+ *
+ * ============================================================================
+ *
+ * <b> REVISION HISTORY </b>
+ *
+ * @Version 1.0 : 22-MAY-2019 
+ * Author :Parag Jain 
+ *                                          
+ *
+ *
+ *============================================================================
+ */
+
+
+
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Button,Alert } from 'react-native';
 import { App_Button } from '../component/App_Button';
@@ -11,7 +43,7 @@ class MainScreen extends Component {
           <App_Button title="Play with friend" navigation={this.props.navigation} RedirectTo='CreateGameScreen' mode='1'/>
           <App_Button title="Toss" navigation={this.props.navigation} RedirectTo='TossScreen' mode='1'/>
           <App_Button title="Spineer" navigation={this.props.navigation} RedirectTo='SpinnerScreen' mode='1'/>
-          <App_Button title="Connection" navigation={this.props.navigation} RedirectTo='Connection' mode='1'/>
+          <App_Button title="WebRtc" navigation={this.props.navigation} RedirectTo='WebRtcScreen' mode='1'/>
           <App_Button title="Exit"  mode='0'/>
     </View>
         );
