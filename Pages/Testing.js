f13fd5aa4b495ed9bd883c43dcdb483f12a02abf
Navneet Singh <navneetofficial25@gmail.com>
2020-05-30 13:45:30 +0530

    Following Changes 1. new branch

--- Pages/Testing.js
+++ Pages/Testing.js
@@ -1,32 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text ,Button} from 'react-native';
-import io from 'socket.io-client/dist/socket.io';
 
 
-var socket = io('http://13.52.248.221:5000'); 
 export  class Testing extends Component {
 
-    
-  componentDidMount() {
-    
-
-    
-    socket.on('send', msg => {
-        alert(msg)
-    });
-
-
-  }
-
-  send(){
-    socket.emit('send','hi');
-  }
   render() {
     return (
       <View>
         <Text>  </Text>
-
-        <Button title="send" onPress={this.send} />
       </View>
     );
   }
--- android/app/src/main/java/com/project/MainApplication.java
+++ android/app/src/main/java/com/project/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +35,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+        @Override
+        protected @Nullable String getBundleAssetName() {
+          return "app.bundle";
+    }
       };
 
   @Override
